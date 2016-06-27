1d5c8b1f45ae464a0b753e875f262168cb5d2f16
Volodymyr Lukashevych <v.lukashevych@gmail.com>
2016-06-27 13:42:07 -0700

    configs for ConEmu and ginconfig

--- vagrant/shell/provision.ps1
+++ vagrant/shell/provision.ps1
@@ -1,8 +1,8 @@
-Configuration TestConfig 
+Configuration WinServerConfig
 { 
-  #param ($MachineName)
+  param ($MachineName)
 
-  Node "localhost"
+  Node $MachineName
   { 
 
 	# Web Server (IIS)
@@ -22,22 +22,43 @@ Configuration TestConfig
     #Install ASP.NET 4.5 
     WindowsFeature ASP 
     { 
-      Ensure = “Present” 
-      Name = “Web-Asp-Net45” 
+		Ensure = "Present" 
+		Name = "Web-Asp-Net45" 
     } 
+	
+	#Desktop experience
+    WindowsFeature DesktopExperience
+	{ 
+		Ensure = "Present" 
+		Name = "Desktop-Experience"
+	}
   } 
 }
 
-TestConfig
+WinServerConfig -MachineName "localhost"
 
-Start-DscConfiguration –Wait -Path .\TestConfig –Verbose
+Start-DscConfiguration -Wait -Path .\WinServerConfig -Verbose -Force
+
+#cinst boxstarter -force -y
+
+Set-WindowsExplorerOptions -EnableShowHiddenFilesFoldersDrives -EnableShowFileExtensions
+Set-TaskbarOptions -Size Small -Lock -Combine Never
+
+#cinst powershell-packagemanagement -y
+(new-object Net.WebClient).DownloadString("http://psget.net/GetPsGet.ps1") | iex
+install-module PsReadLine
+
+cinst conemu -y
+#cinst poshgit -y
+
+
+cinst 7zip.install -y
+cinst executor -y
+
+cinst nodejs.install -y
+cinst git.install -y
+cinst kdiff3 -y
+cinst notepadplusplus.install -y
+cinst visualstudiocode -y
 
 cinst googlechrome -y
-cinst nodejs.install -y
-cinst 7zip.install -y
-cinst git.install -y
-cinst notepadplusplus.install -y
-cinst conemu -y
-cinst poshgit -y
-
-cinst visualstudio2015community -y
